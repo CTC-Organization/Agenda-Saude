bc800a5b3ad1653b610222281223d1da7102fd14
lukajlp <joaolucas.psantana@gmail.com>
2024-07-24 09:36:39 -0300

    ajustes login e tela perfil

--- src/Components/input.tsx
+++ src/Components/input.tsx
@@ -4,7 +4,7 @@ import { View, TextInput, TextInputProps } from "react-native";
 
 function Input({ children }: { children: ReactNode }) {
   return (
-    <View className="w-full h-12 flex-row items-center gap-3 p-3 border border-gray-91 bg-gray-91 rounded-md">
+    <View className="w-full h-12 flex-row items-center gap-3 p-3 border border-gray-91 bg-gray-95 rounded-md">
       {children}
     </View>
   );

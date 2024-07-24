103b44e6af76a3900fdd260642296d83bfdfed91
lukajlp <joaolucas.psantana@gmail.com>
2024-07-24 09:36:39 -0300

    ajustes login e tela perfil

--- src/Components/button.tsx
+++ src/Components/button.tsx
@@ -7,10 +7,12 @@ type Props = PressableProps & {
 export function Button({ title, ...rest }: Props) {
   return (
     <Pressable
-      className="w-full h-14 bg-green-light items-center justify-center rounded-xl"
+      style={{ elevation: 5 }}
+      className="w-full h-14 bg-green-light items-center justify-center 
+      rounded-3xl border-2 border-black"
       {...rest}
     >
-      <Text className="text-white text-base font-normal">{title}</Text>
+      <Text className="text-white text-lg font-normal">{title}</Text>
     </Pressable>
   );
 }
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,33 +1,52 @@
 import { Input } from "@/Components/input";
 import { router } from "expo-router";
-import React from "react";
-import { View, Text, Pressable, Image } from "react-native";
+import { useState } from "react";
+import { View, Text, Pressable, Alert } from "react-native";
 import { Button } from "@/Components/button";
 import Checkbox from "@/Components/checkbox";
 
-const handleLogin = () => {
-  router.replace("(tabs)");
-};
-
 export default function Login() {
+  const [cpf, setCpf] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordVisible, setPasswordVisible] = useState(false);
+
+  const handleLogin = () => {
+    if (cpf.trim() === "" || password.trim() === "") {
+      Alert.alert("Erro", "Preencha todos os campos");
+      return;
+    }
+    router.replace("(tabs)");
+  };
+
+  const togglePasswordVisibility = () => {
+    setPasswordVisible(!passwordVisible);
+  };
+
   return (
     <View className="flex-1 bg-white items-center justify-center p-8">
-      <View className="w-full gap-3 justify-center">
+      <View className="w-full gap-3">
         <Input>
-          <Input.Field placeholder="CPF" />
+          <Input.Field placeholder="CPF" value={cpf} onChangeText={setCpf} />
         </Input>
         <Input>
-          <Input.Field placeholder="Senha" />
-          <Pressable>
-            <Text className="text-green-light">Mostrar</Text>
+          <Input.Field
+            placeholder="Senha"
+            value={password}
+            onChangeText={setPassword}
+            secureTextEntry={!passwordVisible}
+          />
+          <Pressable onPress={togglePasswordVisibility}>
+            <Text className="text-green-light">
+              {passwordVisible ? "Esconder" : "Mostrar"}
+            </Text>
           </Pressable>
         </Input>
-        <View className="w-full items-center justify-center	">
+        <View className="w-full items-center justify-center">
           <Checkbox text="Conectar Automaticamente" />
         </View>
       </View>
       <View className="mt-36"></View>
-      <View className="flex-1 w-full mt-12">
+      <View className="flex-1 w-96 items-center mt-12">
         <Button title="Fazer Login" onPress={handleLogin} />
       </View>
     </View>

569b73095b040e65db298b67870ee32fc02d4b76
Giovanni-LN <118299886+Giovanni-LN@users.noreply.github.com>
2024-08-04 21:34:36 -0300

    AppointmentsScreen-components_Fix

--- src/app/(tabs)/DetailsScreen.tsx
+++ src/app/(tabs)/DetailsScreen.tsx
@@ -1,6 +1,6 @@
 import { Text, View } from "react-native";
 
-export default function DetalhesScreen() {
+export default function DetailsScreen() {
   return (
     <View className="flex-1 justify-center items-center bg-white">
       <Text className="text-2xl font-bold">Detalhes da Consulta</Text>
